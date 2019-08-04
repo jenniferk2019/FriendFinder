f150ae409918f784359f5ab19a5dd99d23b61547
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-08-04 15:52:57 -0400

    javascript survey functions and friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,88 @@
+var pawFriends = [
+	{
+		name: "Goldendoodle",
+		photo: "https://thehappypuppysite.com/wp-content/uploads/2018/06/goldendoodle.jpg",
+        scores: [
+            1, 
+            1, 
+            1, 
+            5, 
+            2, 
+            1, 
+            3, 
+            5, 
+            1, 
+            2
+        ]
+    },
+    
+	{
+		name: "Shiba-Inu",
+		photo: "https://japaninsides.com/wp-content/uploads/2017/11/cute-dog-shiba-inu-ryuji-japan-28.jpg",
+        scores: [
+            1, 
+            2, 
+            3, 
+            2, 
+            5, 
+            4, 
+            3, 
+            3, 
+            3, 
+            2
+        ]
+    },
+    
+	{
+		name: "French-Bulldog",
+		photo: "http://www.frenchbulldogbreed.net/wp-content/uploads/2018/06/The-magnificent-appeal-of-rare-Blue-French-Bulldogs-01.jpg",
+		scores: [
+            4, 
+            4, 
+            4, 
+            3, 
+            2, 
+            1, 
+            5, 
+            5, 
+            2, 
+            1
+        ]
+    },
+    
+	{
+		name: "Mutt",
+		photo: "https://pbs.twimg.com/profile_images/748677687284236289/14nr-AV4.jpg",
+		scores: [
+            5, 
+            4, 
+            3, 
+            2, 
+            1, 
+            1, 
+            2, 
+            3, 
+            4, 
+            5
+        ]
+    },
+    
+	{
+		name: "Snoopy",
+		photo: "http://www.geekcals.com/wp-content/uploads/2015/09/Dancing-Snoopy.jpg",
+		scores: [
+            1, 
+            2, 
+            3, 
+            4, 
+            5, 
+            1, 
+            2, 
+            3, 
+            4, 
+            5
+        ]
+	}
+];
+
+module.exports = pawFriends;
